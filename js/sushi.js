2cd2b7f966c2b74b6ba913db188029cd81e0ccc9
Dale Fenton <fentondale@gmail.com>
2016-02-17 11:10:00 -0500

    sushi layout complete

--- js/sushi.js
+++ js/sushi.js
@@ -1,33 +1,49 @@
 var i;
 var tabs = document.getElementsByClassName('tab-link');
-var active, activeTab, unHideTab, hideTab;
-var tab, unHideId;
-console.log(tabs);
+var hoverInfo = document.querySelector('.hover-info');
+console.log( hoverInfo);
+// console.log(tabs);
 
 function changeTab(){
-  console.log(this);
+  //set functions
+  var active, unHideTab, hideTab;
+  var tab, unHideId;
+  // console.log(this);
   tab = this.querySelector('.control');
   active = document.querySelector('.icon-active');
   active.classList.remove('icon-active');
   active.classList.add('icon-hidden');
-  console.log( tab );
+  // console.log( tab );
   tab.classList.add('icon-active');
   tab.classList.remove('icon-hidden');
-  unHideId = '#' + tab.id.slice(4, tab.id.length);
-  console.log(unHideId);
+  unHideId = '#' + tab.id.slice(8, tab.id.length);
+  // console.log(unHideId);
   unHideTab = document.querySelector(unHideId);
   hideTab = document.querySelector('.tab-active');
-  console.log(unHideTab);
-  console.log(hideTab);
+  // console.log(unHideTab);
+  // console.log(hideTab);
   hideTab.style.display = 'none';
   hideTab.classList.remove('tab-active');
   hideTab.classList.add('tab-hidden');
   unHideTab.style.display = 'block';
   unHideTab.classList.remove('tab-hidden');
   unHideTab.classList.add('tab-active');
-
 }
 
 for(i=0; i < tabs.length; i++){
   tabs[i].addEventListener('click', changeTab);
 }
+
+function showHover(){
+  document.querySelector('#hover-display').innerHTML = this.attributes.hovertext.value;
+  document.querySelector('#hover-display').style.padding = '10px';
+  console.dir(this);
+  console.log(this.attributes.hovertext.value);
+}
+
+function hideHover(){
+  document.querySelector('#hover-display').innerHTML = '';
+  document.querySelector('#hover-display').style.padding = '0';
+}
+hoverInfo.addEventListener('mouseenter', showHover );
+hoverInfo.addEventListener('mouseleave', hideHover );
